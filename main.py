d47d66c204921c3c84499f155398a0a3933bd8b2
Vinícius Meireles Pereira Santos <131965268+vinimeirelres@users.noreply.github.com>
2024-04-30 21:09:15 +0000

    0.0.0.0.3

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import automato
 
-automato.autbase.inicial='c'
+automato.autbase.alfabeto = input("Digite o alfabeto do automato")
+automato.autbase.conjuntoaceitacao = input("Digite o conjunto aceitacao do automato")
+automato.autbase.estados = input("Digite os estados do automato")
+automato.autbase.inicial = input("Digite o estado inicial do automato")
+automato.autbase.transicoes = input("Digite as transicoes do automato")
+
+print(automato.autbase.transicoes, automato.autbase.alfabeto, automato.autbase.conjuntoaceitacao, automato.autbase.estados, automato.autbase.inicial)
